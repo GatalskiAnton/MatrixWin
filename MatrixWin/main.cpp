a26c353eb03a4b68672f8adf4323d9af74bb13c4
Anton <ag629331@gmail.com>
2023-01-13 15:11:35 +0300

    initial commit

---
+++ MatrixWin/main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <windows.h>
+#include <mutex>
+
+
+struct  Params
+{
+	
+	Params(const std::vector<std::vector<int>>& m1, const std::vector<std::vector<int>>& m2, std::vector<std::vector<int>>& resM, int blockI, int blockJ, int matrixSize, int blockSize) 
+		: m1(m1), m2(m2), resM(resM), blockI(blockI), blockJ(blockJ), matrixSize(matrixSize), blockSize(blockSize)
+	{
+	}
+
+	const std::vector<std::vector<int>>& m1;
+	const std::vector<std::vector<int>>& m2;
+	std::vector<std::vector<int>>& resM;
+	int blockI;
+	int blockJ;
+	int matrixSize;
+	int blockSize;
+};
+
+std::vector<double> threadResults;
+std::vector<double> seqResults;
+
+std::mutex g_lock;
+
+void blocksMul(const std::vector <std::vector <int>>& m1, const std::vector <std::vector <int>>& m2, std::vector < std::vector <int>>& resM, int blockI, int blockJ, int matrixSize, int blockSize)
+{
+	for (int i = blockI; i < (std::min)(blockI + blockSize, matrixSize); ++i)
+		for (int j = blockJ; j < (std::min)(blockJ + blockSize, matrixSize); ++j)
+			for (int k = 0; k < matrixSize; ++k)
+				resM[i][j] += m1[i][k] * m2[k][j];
+
+}
+
+void defaultMul(const std::vector <std::vector <int>>& m1, const std::vector <std::vector <int>>& m2, std::vector < std::vector <int>>& resM, int matrixSize, int blockSize)
+{
+	for (int blockI = 0; blockI < matrixSize; blockI += blockSize)
+		for (int blockJ = 0; blockJ < matrixSize; blockJ += blockSize)
+			blocksMul(m1, m2, resM, blockI, blockJ, matrixSize, blockSize);
+}
+
+DWORD WINAPI blocksMul (LPVOID params)
+{
+	Params* paramsPtr = (Params*)params;
+	blocksMul(paramsPtr->m1, paramsPtr->m2, paramsPtr->resM, paramsPtr->blockI, paramsPtr->blockJ, paramsPtr->matrixSize, paramsPtr->blockSize);
+	ExitThread(0);
+}
+
+void threadMul(const std::vector <std::vector <int>>& m1, const std::vector <std::vector <int>>& m2, std::vector < std::vector <int>>& resM, int matrixSize, int blockSize)
+{
+	std::vector<HANDLE> threads;
+	
+	for (int blockI = 0; blockI < matrixSize; blockI += blockSize)
+		for (int blockJ = 0; blockJ < matrixSize; blockJ += blockSize)
+		{
+			g_lock.lock();
+			threads.emplace_back(CreateThread(NULL, 0, &blocksMul, new Params(m1, m2, resM, blockI, blockJ, matrixSize, blockSize),0,NULL));
+			g_lock.unlock();
+		}
+
+	for (HANDLE &thread: threads )
+		WaitForSingleObject(thread, INFINITE);
+	
+	threads.clear();
+}
+
+void getMatrix(std::vector< std::vector<int>>& m1, std::string file)
+{
+	std::ifstream input(file);
+	std::string row;
+	int k = 0;
+	while (getline(input, row))
+	{
+		std::stringstream ss;
+		ss.str(row);
+		std::string value;
+
+		while (ss >> value)
+			m1[k].push_back(std::stoi(value));
+		
+		++k;
+	}
+}
+
+void test(std::vector< std::vector<int>>& m1, std::vector< std::vector<int>>& m2, std::vector< std::vector<int>>& m3, int matrixSize)
+{
+	std::ofstream out("output.txt");
+	auto startTimeThread = std::chrono::high_resolution_clock::now();
+	for (int i = 1; i < matrixSize; ++i)
+	{
+		threadMul(m1, m2, m3, matrixSize, i);
+		auto tempTime = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> tempDuration = tempTime - startTimeThread;
+		threadResults.push_back(tempDuration.count());
+	}
+	auto endTimeThread = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> durationThread = endTimeThread - startTimeThread;
+	std::cout << durationThread.count() << '\n';
+
+	auto startTimeSeq = std::chrono::high_resolution_clock::now();
+	for (int i = 1; i < matrixSize; ++i)
+	{
+		defaultMul(m1, m2, m3, matrixSize, i);
+		auto tempTime = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<double> tempDuration = tempTime - startTimeSeq;
+		seqResults.push_back(tempDuration.count());
+	}
+	auto endTimeSeq = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> durationSeq = endTimeSeq - startTimeSeq;
+	std::cout << durationSeq.count() << '\n';
+	out << "------------------------------------\n";
+	for (int i = 0; i < threadResults.size(); ++i)
+	{
+		out << "| Block size " << i + 1 << ", difference " << seqResults[i] / threadResults[i] << '\n';
+		out << "------------------------------------\n";
+	}
+	out << "final difference " << durationSeq.count() / durationThread.count() << '\n';
+
+}
+
+int main()
+{
+	int matrixSize = 300;
+	std::vector< std::vector<int>> m1(matrixSize);
+	std::vector< std::vector<int>> m2(matrixSize);
+	std::vector< std::vector<int>> m3(matrixSize, std::vector<int>(matrixSize));
+	getMatrix(m1, "matrix1.txt");
+	getMatrix(m2, "matrix2.txt");
+	test(m1, m2, m3, matrixSize);
+}
